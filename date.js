8b2eca289c2216fccea0b20a4ddf53052cc43d45
Your Name <you@example.com>
2021-05-20 13:30:35 +0300

    which day was 8 days ago from now, and 30 min ago from now

--- date.js
+++ date.js
@@ -1,21 +1,29 @@
 const subDays = require("date-fns/subDays");
 const format = require("date-fns/format");
+const isPast = require("date-fns/isPast");
 const now = new Date();
 
-console.log("now", now);
+// console.log("now", now);
 
 const rez = subDays(now, 1);
 const formatedDate = format(rez, "yyyy-LLLL-do, EEEE, HH:mm:ss");
+console.log("----------------------------------------");
 console.log("formatedDate", formatedDate);
-const rez8DaysBefore = subDays(now, 8);
+console.log("----------------------------------------");
 // console.log("Vakar dienos dabartinis momentas yra", rez.toLocaleDateString(), rez.toLocaleTimeString());
 // -------------------------------------------------------------------
 
 // / 2.1 gauti kelintadienis buvo pries 8nias dienas
 
-const formatedDate8DaysBefore = format;
-// console.log("Pries 8 dienas", rez8DaysBefore.toLocaleDateString(), rez8DaysBefore.toLocaleTimeString());
+const rez8DaysBefore = subDays(now, 8);
+const formatedDate8DaysBefore = format(rez8DaysBefore, "yyyy-LLLL-do, EEEE, HH:mm:ss");
+console.log("formatedDate8DaysBefore", formatedDate8DaysBefore);
+console.log("----------------------------------------");
 
 // // 2.2 gauti jei imanoma zmogisku laiku praeities laika minutemis
-
-// // pvz 5 min ago, 2 hours ago ir pan
+const minutesToAdd = 30;
+const currentDate = new Date();
+const futureDate = new Date(currentDate.getTime() - minutesToAdd * 60000);
+const format30minAgo = format(futureDate, "yyyy-LLLL-do, EEEE, HH:mm:ss");
+console.log("format in 30 min ago", format30minAgo);
+console.log("----------------------------------------");
