cbdd8dd55ff73fc42ea4b3db8c28353659a52734
Your Name <you@example.com>
2021-05-20 09:17:46 +0300

    formated date

--- date.js
+++ date.js
@@ -1,8 +1,21 @@
-var subDays = require("date-fns/subDays");
-
+const subDays = require("date-fns/subDays");
+const format = require("date-fns/format");
 const now = new Date();
 
 console.log("now", now);
 
 const rez = subDays(now, 1);
-console.log("Vakar dienos dabartinis momentas yra", rez.toLocaleDateString(), rez.toLocaleTimeString());
+const formatedDate = format(rez, "yyyy-LLLL-do, EEEE, HH:mm:ss");
+console.log("formatedDate", formatedDate);
+const rez8DaysBefore = subDays(now, 8);
+// console.log("Vakar dienos dabartinis momentas yra", rez.toLocaleDateString(), rez.toLocaleTimeString());
+// -------------------------------------------------------------------
+
+// / 2.1 gauti kelintadienis buvo pries 8nias dienas
+
+const formatedDate8DaysBefore = format;
+// console.log("Pries 8 dienas", rez8DaysBefore.toLocaleDateString(), rez8DaysBefore.toLocaleTimeString());
+
+// // 2.2 gauti jei imanoma zmogisku laiku praeities laika minutemis
+
+// // pvz 5 min ago, 2 hours ago ir pan
